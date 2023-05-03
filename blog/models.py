8e519574ae1c91d183351f665b1cf21f12fc07f2
gagdoc <gagdoc@gmail.com>
2023-05-03 13:57:05 +0900

    모델 생성하고 시간 설정하기

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author : 나중에 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
+
